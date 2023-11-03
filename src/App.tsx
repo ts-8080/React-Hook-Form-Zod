19b0ff90f1528be3c9390f74c1e2d94f4a264895
ryuji abe <ryuji-abe@Room335.local>
2023-11-04 02:11:24 +0900

    add zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,56 @@
-import { Box, Button, Center, ChakraProvider, Input, Text } from "@chakra-ui/react";
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import "./App.css";
+import { useForm } from "react-hook-form";
+import { ReactNode } from "react";
+import { validationSchema } from "./utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
 
 function App() {
-  return (
-    <ChakraProvider>
-      
-      <Center  className="form-container">
-        
-        <Card minW={'md'} minH={'md'} p={10} mt={150}>
-          <Text textAlign={'center'} fontSize='4xl'>Login Form</Text>
-          <label htmlFor="名前">名前</label>
-          <Input variant='outline' placeholder='name' />
-          <label htmlFor="メールアドレス">メールアドレス</label>
-          <Input variant='outline' placeholder='email' />
-          <label htmlFor="パスワード">パスワード</label>
-          <Input variant='filled' placeholder='password' />
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
 
-          <Button mt={100} colorScheme='blue'>送信</Button>
-        </Card>
-      </Center>
-      
-    </ChakraProvider>
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="form-container">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="名前">名前</label>
+        <input
+          type="text"
+          {...register("name")}
+        />
+        <p>{errors.name?.message as ReactNode} </p>
+        <label htmlFor="メールアドレス">メールアドレス</label>
+        <input
+          type="email"
+          {...register("email")}
+        />
+        <p>{errors.email?.message as ReactNode} </p>
+        <label htmlFor="パスワード">パスワード</label>
+        <input
+          type="password"
+          {...register("password")}
+        />
+        <p>{errors.password?.message as ReactNode} </p>
+
+        <button type="submit">送信</button>
+      </form>
+    </div>
   );
 }
 
